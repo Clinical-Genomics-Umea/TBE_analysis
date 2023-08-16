97cf2473e1fbafbe89f06be4ceab7e14f62a599f
William Rosenbaum <william.rosenbaum88@gmail.com>
2023-08-16 10:08:21 +0200

    bugs

--- Snakefile.py
+++ Snakefile.py
@@ -4,22 +4,21 @@ import re
 # COMMON FUNCTIONS
 
 def get_samples(sample_dir: str) -> list[str]:
-    BLACKLIST = ["",]
-
     return [
         x.stem for x in Path(SAMPLE_DIR).iterdir() 
         if x.is_dir() 
-        and x.stem not in BLACK_LIST
         and not x.stem.startswith(".")
     ]
 
 def get_fastx(wildcards):
     in_folder = Path(f"{SAMPLE_DIR}/{wildcards.sample}")
-    return [file for file in Path(in_folder).iterdir() if re.search(r"fq|fastq|fa|fasta|fna", file.name)]
+    files = [file for file in Path(in_folder).iterdir() if re.search(r"fq|fastq|fa|fasta|fna", file.name)]
+    print(files)
+    return files
 
 
 # IO
-configfile: "/config.yaml"
+configfile: "config.yaml"
 
 RESULTS = config["RESULTS"]
 SAMPLE_DIR = config["SAMPLES"]
@@ -48,20 +47,19 @@ rule concatenate_fastq:
 
 rule porechop:
     input:
-        fastq=get_fastq
+        fastq=get_fastx
     output:
-        chopped=f"{RESULTS}/{{sample}}/{sample}}_raw_porechop.fastq.gz",
-        log=f"{OUT_DIR}/{{sample}}/read_statistics/{{sample}}_raw_porechop_stats.txt",
-    params:
-        porechop=config["PORECHOP"],
+        chopped=f"{RESULTS}/{{sample}}/{{sample}}_raw_porechop.fastq.gz",
+    log:
+        f"{RESULTS}/{{sample}}/logs/log_porechops.txt",
     message:
         """
         Running porechop on {input.fastq}
         """
     shell:
         """
-        {params.porechop}/porechop-runner.py \
+        porechop \
         -i {input.fastq} \
         -o {output.chopped} \
-        | tee {output.log}
+        | tee {log}
         """
